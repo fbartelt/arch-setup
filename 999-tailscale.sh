c0adfff05b17bb147b39fa043e09b1627dde643d
fbartelt <fbarteltp@gmail.com>
2026-07-23 17:42:06 -0300

    feat: add tailscale configuration
    This is optional and was created to connect local->macro computer

---
+++ 999-tailscale.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+installpac(){
+    if pacman -Qi $1 &> /dev/null; then
+        echo "Package {$1} is already installed."
+    else
+        sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+echo "Installing tailscale"
+installpac "tailscale"
+
+sudo systemctl enable --now tailscaled
+
+sudo tailscale up
+
+echo "Reboot your system."
