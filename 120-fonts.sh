e86e4eb4b2ae7a820a126cd0f0a5ca5acd19d920
fbartelt <fbarteltp@gmail.com>
2026-07-24 11:34:13 -0300

    fix: correct local fonts path

--- 120-fonts.sh
+++ 120-fonts.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e  # Exit on any error
 installpac(){
-    if pacman -Qi $1 &> /dev/null; then
-        echo "Package {$1} is already installed."
+    if pacman -Qi "$1" &> /dev/null; then
+        echo "Package $1 is already installed."
     else
-        sudo pacman -S --noconfirm --needed $1
+        sudo pacman -S --noconfirm --needed "$1"
     fi
 }
-
 installyay(){
-    if pacman -Qi $1 &> /dev/null; then
-        echo "Package {$1} is already installed."
+    if pacman -Qi "$1" &> /dev/null; then
+        echo "Package $1 is already installed."
     else
-        yay -S --noconfirm --needed $1
+        yay -S --noconfirm --needed "$1"
     fi
 }
 
@@ -41,26 +40,26 @@ for name in "${list_yay[@]}" ; do
 done
 
 echo "Installing MPlus fonts from github"
-mkdir -p .local/share/fonts/ttf/MPlus
-mkdir -p .local/share/fonts/otf/MPlus
-mkdir -p .local/share/fonts/woff2/MPlus
+mkdir -p "$HOME/.local/share/fonts/ttf/MPlus"
+mkdir -p "$HOME/.local/share/fonts/otf/MPlus"
+mkdir -p "$HOME/.local/share/fonts/woff2/MPlus"
 
 git clone https://github.com/coz-m/MPLUS_FONTS.git /tmp/MPLUS_FONTS
 
 # Copy all .ttf files from any ttf/ subdirectory
-find /tmp/MPLUS_FONTS/fonts -type f -path '*/ttf/*.ttf' -exec cp {} .local/share/fonts/ttf/MPlus/ \;
-find /tmp/MPLUS_FONTS/fonts -type f -path '*/variable/*.ttf' -exec cp {} .local/share/fonts/ttf/MPlus/ \;
+find /tmp/MPLUS_FONTS/fonts -type f -path '*/ttf/*.ttf' -exec cp {} "$HOME/.local/share/fonts/ttf/MPlus" \;
+find /tmp/MPLUS_FONTS/fonts -type f -path '*/variable/*.ttf' -exec cp {} "$HOME/.local/share/fonts/ttf/MPlus" \;
 
 # Copy all .otf files from any otf/ subdirectory
-find /tmp/MPLUS_FONTS/fonts -type f -path '*/otf/*.otf' -exec cp {} .local/share/fonts/otf/MPlus/ \;
+find /tmp/MPLUS_FONTS/fonts -type f -path '*/otf/*.otf' -exec cp {} "$HOME/.local/share/fonts/otf/MPlus" \;
 
 # Copy all .woff2 files from any otf/ subdirectory
-find /tmp/MPLUS_FONTS/fonts -type f -path '*/webfonts/*.woff2' -exec cp {} .local/share/fonts/otf/MPlus/ \;
+find /tmp/MPLUS_FONTS/fonts -type f -path '*/webfonts/*.woff2' -exec cp {} "$HOME/.local/share/fonts/woff2/MPlus" \;
 
 rm -rf /tmp/MPLUS_FONTS
 
-chmod -R a+rX .local/share/fonts/ttf/MPlus
-chmod -R a+rX .local/share/fonts/otf/MPlus
-chmod -R a+rX .local/share/fonts/woff2/MPlus
+chmod -R a+rX "$HOME/.local/share/fonts/ttf/MPlus"
+chmod -R a+rX "$HOME/.local/share/fonts/otf/MPlus"
+chmod -R a+rX "$HOME/.local/share/fonts/woff2/MPlus"
 
 fc-cache
